cfa9b76e8591874de0e18783e291b610e5f80acf
tsarcevic <tsarcevic@etfos.hr>
2017-10-26 15:07:57 +0200

    added welcome screen

--- app/src/main/java/com/example/cobeosijek/premierleague/WelcomeScreen.java
+++ app/src/main/java/com/example/cobeosijek/premierleague/WelcomeScreen.java
@@ -2,8 +2,21 @@ package com.example.cobeosijek.premierleague;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class WelcomeScreen extends AppCompatActivity {
+
+    @BindView(R.id.premier_league_crest)
+    ImageView premierLeagueCrest;
+
+    @BindView(R.id.welcome_screen_text)
+    TextView welcomeScreenText;
+
+    @BindView(R.id.forward_button)
+    ImageView forwardButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
